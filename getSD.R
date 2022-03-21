1cea8668c51da847a1b6c3b601cd7430f7ea6f34
AgedMordorBlue <y.meiborg@gmail.com>
2022-03-21 18:00:51 +0100

    Update getSD.R

--- getSD.R
+++ getSD.R
@@ -1,6 +1,10 @@
 setwd("/scratch/bell/ymeiborg")
 source("./model_function.R")
 
+###############################
+############ get data #########
+###############################
+
 load("fig2a/Fig2a.Rdata")
 
 haploRep_2a <- as_tibble(modelOutput) %>% 
@@ -14,11 +18,6 @@ haploRep_2a$strategy = factor(haploRep_2a$strategy,
 haploRep_2a$`Release` = factor(haploRep_2a$`Release`, 
                             levels = c(1,2),
                             labels = c("Females","Males"))
-
-haploRepSD_2a <- filter(haploRep_2a, strategy == "Neutral" | strategy == "Female infertility", generation == 7) %>%
-  group_by(strategy, `Release`, Allele) %>%
-  summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
-  filter(Allele == "GD")
 
 load("fig2b/Fig2b.Rdata")
 
@@ -34,12 +33,6 @@ haploRep_2b$`Release` = factor(haploRep_2b$`Release`,
                                levels = c(1,2),
                                labels = c("Females","Males"))
 
-haploRepSD_2b <- filter(haploRep_2b, strategy == "Neutral" | strategy == "Female infertility", generation == 7) %>%
-  group_by(strategy, `Release`, Allele) %>%
-  summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
-  filter(Allele == "GD")
-
-
 load("figs1a/FigS1a.Rdata")
 
 haploRep_s1a <- as_tibble(modelOutput) %>% 
@@ -54,10 +47,6 @@ haploRep_s1a$`Release` = factor(haploRep_s1a$`Release`,
                                levels = c(1,2),
                                labels = c("Females","Males"))
 
-haploRepSD_s1a <- filter(haploRep_s1a, strategy == "Neutral" | strategy == "Female infertility", generation == 7) %>%
-  group_by(strategy, `Release`, Allele) %>%
-  summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
-  filter(Allele == "GD")
 
 load("figs1b/FigS1b.Rdata")
 
@@ -73,7 +62,32 @@ haploRep_s1b$`Release` = factor(haploRep_s1b$`Release`,
                                 levels = c(1,2),
                                 labels = c("Females","Males"))
 
-haploRepSD_s1b <- filter(haploRep_s1b, strategy == "Neutral" | strategy == "Female infertility", generation == 7) %>%
-  group_by(strategy, `Release`, Allele) %>%
+haploRep <- bind_rows(haploRep_2a, haploRep_2b, haploRep_s1a, haploRep_s1b) %>%
+  mutate(dataset = rep(c("2a", "2b", "s1a", "s1b"), each = nrow(haploRep_2a)))
+
+##################################################
+############## SD at generation 7 ################
+##################################################
+
+haploRepSD_g7 <- filter(haploRep, strategy == "Neutral" | strategy == "Female infertility", generation == 7) %>%
+  group_by(strategy, `Release`, Allele, dataset) %>%
   summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
   filter(Allele == "GD")
+
+##################################################
+############## SD at generation 10 ###############
+##################################################
+
+haploRepSD_g10 <- filter(haploRep, strategy == "Neutral" | strategy == "Female infertility", generation == 10) %>%
+  group_by(strategy, `Release`, Allele, dataset) %>%
+  summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
+  filter(Allele == "GD")
+
+#####################################
+############## max SD ###############
+#####################################
+
+haploRepSD_max <- filter(haploRep, strategy == "Neutral" | strategy == "Female infertility") %>%
+  group_by(strategy, `Release`, Allele, dataset, generation) %>%
+  summarise(mean = mean(Frequency), sd = sd(Frequency)) %>%
+  filter(sd == max(sd), Allele == "GD")
