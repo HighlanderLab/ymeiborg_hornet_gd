1a9b833184eec70bc756e7a1a25c50f215811976
AgedMordorBlue <92516260+AgedMordorBlue@users.noreply.github.com>
2022-04-05 18:32:10 +0200

    extended sensitivity analysis
    added intermediate parameters and combined plots. Also fixed the error bars so they are scaled to the x-axes.

--- figs2/figs2.R
+++ figs2/figs2.R
@@ -28,8 +28,8 @@ input$gdSex <- "F" #which sex carries the gene drive F or M
 input$nGD <- 100 #number of gene drive carrying animals to introduce
 input$strategy <- 3 #what targeting strategy to use 1 = neutral, 2 = male, 3 = female
 input$pnhej <- c(0, 0.02) #probability of non-homologous end joining, determines the resistance alleles (0.02 in mosquitos)
-input$cutRate <- c(1, 0.95) #propability CRISPR cuts the opposite DNA strand
-input$pHMort <- c(0, 0.1) #mortality of gene drive carriers.
+input$cutRate <- c(1, 0.95, 0.97) #propability CRISPR cuts the opposite DNA strand
+input$pHMort <- c(0, 0.1, 0.15) #mortality of gene drive carriers.
 inputs <- expand.grid(input)
 
 inputs <- inputs %>%
@@ -40,8 +40,9 @@ inputs <- inputs %>%
             (meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & rmax==10 & maxFemMatings==4 & maxMalMatings==2) |
             (meanFemProgeny==300 & meanMalProgeny==300 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4 & maxMalMatings==2) |
             (meanFemProgeny==meanMalProgeny & meanFemMatings==3.275 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4 & maxMalMatings==2)) &
-           ((pnhej == 0 & cutRate == 1 & pHMort == 0) |
-           (pnhej == 0.02 & cutRate == 0.95 & pHMort == 0.1)))
+            ((pnhej == 0 & cutRate == 1 & pHMort == 0) |
+             (pnhej == 0.02 & cutRate == 0.95 & pHMort == 0.1) |
+             (pnhej == 0 & cutRate == 0.97 & pHMort == 0.15)))
 
 #########################################
 ########## Run model ####################
@@ -67,9 +68,7 @@ modelOutput <- apply(modelOutput, 2, c)
 
 save(modelOutput, file = "FigS2.Rdata")
 
-##################
-## ---- Plots ----
-##################
+#################### Prepare data ##################
 
 modelOutput <- as_tibble(modelOutput)
 
@@ -86,25 +85,37 @@ modelOutput <- rowwise(modelOutput) %>%
     TRUE ~ "default"
   ))
 
+PaperTheme <- theme_bw(base_size = 11, base_family = "sans") + 
+  theme(strip.background = element_blank(),
+        panel.grid = element_blank(),
+        title=element_text(size=14, hjust=0.5), 
+        plot.title=element_text(size=14, hjust=0.5),
+        #legend.position="none",
+        axis.title=element_text(size=12))
+
 modelOutputOptimal <- filter(modelOutput, cutRate == 1)
 modelOutputRealistic <- filter(modelOutput, cutRate == 0.95)
+modelOutputMid <- filter(modelOutput, cutRate == 0.97)
 
 year25optimal <- filter(modelOutputOptimal, generation == 25)
 year25realistic <- filter(modelOutputRealistic, generation == 25)
+year25mid<- filter(modelOutputMid, generation == 25)
 
 ntimes <- nrow(year25optimal)/max(inputs$repetitions)
 year25optimal$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
 year25optimal$paramSet <- factor(year25optimal$paramSet)
 year25realistic$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
 year25realistic$paramSet <- factor(year25realistic$paramSet)
+year25mid$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
+year25mid$paramSet <- factor(year25mid$paramSet)
 
 ################## Realistic GD scenario ##################
 
-suppressed <- rowwise(year25realistic) %>%
-  mutate(suppressed = case_when(popSizeF == 0 ~ 1,
+suppressedRealistic <- rowwise(year25realistic) %>%
+  mutate(suppressedRealistic = case_when(popSizeF == 0 ~ 1,
                                 popSizeF > 0 ~ 0)) %>%
   group_by(paramSet) %>%
-  mutate(suppressionRate = sum(suppressed)/10) %>%
+  mutate(suppressionRate = sum(suppressedRealistic)/10) %>%
   distinct(paramSet, .keep_all = TRUE) %>%
   select(paramSet, meanProgeny:maxMalMatings, rmax, suppressionRate, variableRange) %>%
   pivot_longer(cols = meanProgeny:rmax) %>%
@@ -112,34 +123,15 @@ suppressed <- rowwise(year25realistic) %>%
   mutate(name = factor(name), cond = "opt") %>%
   arrange(name)
 
-suppressed$name <- factor(suppressed$name, 
+suppressedRealistic$name <- factor(suppressedRealistic$name, 
                           levels = c('rmax','meanFemMatings','meanMalMatings',
                                      'maxFemMatings','maxMalMatings',
                                      'meanProgeny', 'default'), 
                           labels = c('Max growth rate','Mean female matings','Mean male matings',
                                      'Max female matings','Max male matings',
                                      'Mean progeny', 'Default'))
-suppressed <- suppressed[order(suppressed$name),]
-
-PaperTheme <- theme_bw(base_size = 11, base_family = "sans") + 
-  theme(strip.background = element_blank(),
-        panel.grid = element_blank(),
-        title=element_text(size=14, hjust=0.5), 
-        plot.title=element_text(size=14, hjust=0.5),
-        legend.position="none",
-        axis.title=element_text(size=12))
-
-p1 <- ggplot(data = suppressed) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = suppressionRate, colour = name)) +
-  geom_point(aes(x = value, y = suppressionRate, colour = name)) +
-  scale_colour_manual(values=rep("black",6)) +
-  ylim(c(0,1)) +
-  ylab("Suppression rate") +
-  xlab("Parameter value") + 
-  PaperTheme +
-  ggtitle("Realistic gene drive")
-p1
+suppressedRealistic <- suppressedRealistic[order(suppressedRealistic$name),] %>%
+  mutate(condition = "Realistic")
 
 modelOutputRealistic$paramSet <- rep(1:ntimes, each = nrow(modelOutputRealistic)/ntimes)
 modelOutputRealistic$paramSet <- factor(modelOutputRealistic$paramSet)
@@ -173,29 +165,18 @@ lastGenStatsRealistic <- left_join(x = lastGenStatsRealistic,
                                            'Mean progeny', 'Default')),
          lower = mLastGen - semLastGen,
          upper = mLastGen + semLastGen)
-lastGenStatsRealistic <- lastGenStatsRealistic[order(lastGenStatsRealistic$name),]
-
-p2 <- ggplot(data = lastGenStatsRealistic) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = mLastGen, colour = name)) +
-  geom_point(aes(x = value, y = mLastGen, colour = name)) +
-  geom_errorbar(aes(x = value, y = mLastGen, ymin = lower, ymax = upper), 
-                size = 0.1, width = 0.1) +
-  ylim(c(0,25)) +
-  ylab("Last viable generation") +
-  xlab("Parameter value") +
-  scale_colour_manual(values=rep("black",6)) +
-  PaperTheme +
-  ggtitle("Realistic gene drive")
-p2
+lastGenStatsRealistic <- lastGenStatsRealistic[order(lastGenStatsRealistic$name),] %>%
+  mutate(condition = "Realistic") %>%
+  group_by(name) %>%
+  mutate(wd = (max(value) - min(value)) * 0.1)
 
 ################## Optimal GD scenario ##################
 
-suppressed <- rowwise(year25optimal) %>%
-  mutate(suppressed = case_when(popSizeF == 0 ~ 1,
+suppressedOptimal <- rowwise(year25optimal) %>%
+  mutate(suppressedOptimal = case_when(popSizeF == 0 ~ 1,
                                 popSizeF > 0 ~ 0)) %>%
   group_by(paramSet) %>%
-  mutate(suppressionRate = sum(suppressed)/10) %>%
+  mutate(suppressionRate = sum(suppressedOptimal)/10) %>%
   distinct(paramSet, .keep_all = TRUE) %>%
   select(paramSet, meanProgeny:maxMalMatings, rmax, suppressionRate, variableRange) %>%
   pivot_longer(cols = meanProgeny:rmax) %>%
@@ -203,26 +184,17 @@ suppressed <- rowwise(year25optimal) %>%
   mutate(name = factor(name), cond = "opt") %>%
   arrange(name)
 
-suppressed$name <- factor(suppressed$name, 
+suppressedOptimal$name <- factor(suppressedOptimal$name, 
                           levels = c('rmax','meanFemMatings','meanMalMatings',
                                      'maxFemMatings','maxMalMatings',
                                      'meanProgeny', 'default'), 
                           labels = c('Max growth rate','Mean female matings','Mean male matings',
                                      'Max female matings','Max male matings',
                                      'Mean progeny', 'Default'))
-suppressed <- suppressed[order(suppressed$name),]
+suppressedOptimal <- suppressedOptimal[order(suppressedOptimal$name),] %>%
+  mutate(condition = "Optimal")
 
-p3 <- ggplot(data = suppressed) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = suppressionRate, colour = name)) +
-  geom_point(aes(x = value, y = suppressionRate, colour = name)) +
-  scale_colour_manual(values=rep("black",6)) +
-  ylim(c(0,1)) +
-  ylab("Suppression rate") +
-  xlab("Parameter value") + 
-  PaperTheme +
-  ggtitle("Optimal gene drive")
-p3
+
 
 modelOutputOptimal$paramSet <- rep(1:ntimes, each = nrow(modelOutputOptimal)/ntimes)
 modelOutputOptimal$paramSet <- factor(modelOutputOptimal$paramSet)
@@ -256,25 +228,108 @@ lastGenStatsOptimal <- left_join(x = lastGenStatsOptimal,
                                            'Mean progeny', 'Default')),
          lower = mLastGen - semLastGen,
          upper = mLastGen + semLastGen)
-lastGenStatsOptimal <- lastGenStatsOptimal[order(lastGenStatsOptimal$name),]
+lastGenStatsOptimal <- lastGenStatsOptimal[order(lastGenStatsOptimal$name),] %>%
+  mutate(condition = "Optimal") %>%
+  group_by(name) %>%
+  mutate(wd = (max(value) - min(value)) * 0.1)
 
-p4 <- ggplot(data = lastGenStatsOptimal) +
+################## Intermediate GD scenario ##################
+
+suppressedMid <- rowwise(year25mid) %>%
+  mutate(suppressedMid = case_when(popSizeF == 0 ~ 1,
+                                popSizeF > 0 ~ 0)) %>%
+  group_by(paramSet) %>%
+  mutate(suppressionRate = sum(suppressedMid)/10) %>%
+  distinct(paramSet, .keep_all = TRUE) %>%
+  select(paramSet, meanProgeny:maxMalMatings, rmax, suppressionRate, variableRange) %>%
+  pivot_longer(cols = meanProgeny:rmax) %>%
+  filter(variableRange == name | variableRange == "default") %>%
+  mutate(name = factor(name), cond = "opt") %>%
+  arrange(name)
+
+suppressedMid$name <- factor(suppressedMid$name, 
+                          levels = c('rmax','meanFemMatings','meanMalMatings',
+                                     'maxFemMatings','maxMalMatings',
+                                     'meanProgeny', 'default'), 
+                          labels = c('Max growth rate','Mean female matings','Mean male matings',
+                                     'Max female matings','Max male matings',
+                                     'Mean progeny', 'Default'))
+suppressedMid <- suppressedMid[order(suppressedMid$name),] %>%
+  mutate(condition = "Intermediate")
+
+modelOutputRealistic$paramSet <- rep(1:ntimes, each = nrow(modelOutputRealistic)/ntimes)
+modelOutputRealistic$paramSet <- factor(modelOutputRealistic$paramSet)
+
+lastGenMid <- lastGenMid %>%
+  filter(popSizeF != 0) %>%
+  group_by(paramSet, repetitions) %>%
+  filter(generation == max(generation))
+
+lastGenStatsMid <- group_by(lastGenMid, paramSet) %>%
+  summarise(mLastGen = mean(generation),
+            semLastGen = sd(generation)/sqrt(10)) 
+
+lastGenStatsMid <- left_join(x = lastGenStatsMid, 
+                                   y = lastGenMid, by = "paramSet") %>%
+  distinct(paramSet, .keep_all = TRUE) %>%
+  select(paramSet:semLastGen, meanProgeny:rmax, -k, variableRange) %>%
+  pivot_longer(cols = meanProgeny:rmax) %>%
+  filter(variableRange == name | variableRange == "default") %>%
+  mutate(name = factor(name, levels = c('rmax','meanFemMatings','meanMalMatings',
+                                        'maxFemMatings','maxMalMatings',
+                                        'meanProgeny', 'default'), 
+                       labels = c('Max growth rate','Mean female matings','Mean male matings',
+                                  'Max female matings','Max male matings',
+                                  'Mean progeny', 'Default')), 
+         variableRange = factor(variableRange, levels = c('rmax','meanFemMatings','meanMalMatings',
+                                                          'maxFemMatings','maxMalMatings',
+                                                          'meanProgeny', 'default'), 
+                                labels = c('Max growth rate','Mean female matings','Mean male matings',
+                                           'Max female matings','Max male matings',
+                                           'Mean progeny', 'Default')),
+         lower = mLastGen - semLastGen,
+         upper = mLastGen + semLastGen)
+lastGenStatsMid <- lastGenStatsMid[order(lastGenStatsMid$name),] %>%
+  mutate(condition = "Intermediate") %>%
+  group_by(name) %>%
+  mutate(wd = (max(value) - min(value)) * 0.1)
+
+################## combine data ##################
+
+suppressed <- bind_rows(suppressedRealistic, suppressedOptimal, suppressedMid)
+
+lastGenStats <- bind_rows(lastGenStatsRealistic, lastGenStatsOptimal, lastGenStatsMid)
+
+################## plots ##################
+
+p1 <- ggplot(data = suppressed) +
   facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = mLastGen, colour = name)) +
-  geom_point(aes(x = value, y = mLastGen, colour = name)) +
+  geom_line(aes(x = value, y = suppressionRate, col= condition)) +
+  geom_point(aes(x = value, y = suppressionRate, col = condition)) +
+  scale_colour_manual(values=met.brewer("Greek", n = length(unique(suppressed$condition))),
+                      name = "Gene Drive Conditions") +
+  ylim(c(0,1)) +
+  ylab("Suppression rate") +
+  xlab("Parameter value") + 
+  PaperTheme
+
+p2 <- ggplot(data = lastGenStats) +
+  facet_wrap(. ~ name, scales = "free_x") +
+  geom_line(aes(x = value, y = mLastGen, colour = condition)) +
+  geom_point(aes(x = value, y = mLastGen, colour = condition)) +
   geom_errorbar(aes(x = value, y = mLastGen, ymin = lower, ymax = upper), 
-                size = 0.1, width = 0.1) +
+                size = 0.1, width = lastGenStats$wd) +
+  scale_colour_manual(values=met.brewer("Greek", n = length(unique(lastGenStats$condition))),
+                      name = "Gene Drive Conditions") +
   ylim(c(0,25)) +
   ylab("Last viable generation") +
   xlab("Parameter value") +
-  scale_colour_manual(values=rep("black",6)) +
-  PaperTheme +
-  ggtitle("Optimal gene drive")
-p4
+  PaperTheme
 
-p <- (p1 + p2) / (p3 + p4) + 
-  plot_annotation(tag_levels = "A")
+p <- p1 + p2 + 
+  plot_annotation(tag_levels = "A") +
+  plot_layout(guides = 'collect') & theme(legend.position='bottom')
 p
 
-ggsave(plot = p, filename = "FigS2.png", height = 30, width = 25, unit = "cm")
+ggsave(plot = p, filename = "FigS2.png", height = 20, width = 25, unit = "cm")
 
