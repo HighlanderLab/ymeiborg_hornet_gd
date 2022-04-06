2163de29e2e93b84210526020942c7573710dc8d
AgedMordorBlue <92516260+AgedMordorBlue@users.noreply.github.com>
2022-04-06 17:22:37 +0200

    Update figs4.R

--- figs4/figs4.R
+++ figs4/figs4.R
@@ -2,7 +2,7 @@
 ########## Setup #########
 ##########################
 
-setwd("/scratch/bell/ymeiborg/figs2")
+setwd("/scratch/bell/ymeiborg/figs4")
 
 source("../model_function.R")
 
@@ -15,33 +15,30 @@ input <- list()
 input$repetitions <- 1:10 # number of reps
 input$generations <- 25 #runtime of the simulation
 
-input$meanFemProgeny <- c(10,155,300,445,590) #average of female progeny per queen
-input$meanMalProgeny <- c(10,155,300,445,590) #average of male progeny per queen
-input$meanFemMatings <- c(1.275,2.275,3.275,4.275,5.275) #average number of times females mate
-input$meanMalMatings <- c(0.3,0.6,0.9,1.2,1.5) #average number of times males mate
-input$maxFemMatings <- c(2,3,4,5,6) #maximum number of times females mate
-input$maxMalMatings <- c(1,2,3,4) #maximum number of times males mate
+input$meanFemProgeny <- c(10, 20, 40, 80, 160, 320, 640) #average of female progeny per queen
+input$meanMalProgeny <- c(10, 20, 40, 80, 160, 320, 640) #average of male progeny per queen
+input$meanFemMatings <- c(3.275, 0.2) #average number of times females mate
+input$meanMalMatings <- 0.9 #average number of times males mate
+input$maxFemMatings <- c(4, 2) #maximum number of times females mate
+input$maxMalMatings <- 3 #maximum number of times males mate
 input$k <- 1000 #simulated k --> carrying capacity (Carrying capacity K equals to 10.6/km^2)
-input$rmax <- c(2,6,10,14,18) #growth rate of the population
+input$rmax <- 10 #growth rate of the population
 input$N <- 1000 #size of start WT population
 input$gdSex <- "F" #which sex carries the gene drive F or M
 input$nGD <- 100 #number of gene drive carrying animals to introduce
+input$multiplex <- 1 #how many multiplexes in the gene drives, not used currently
 input$strategy <- 3 #what targeting strategy to use 1 = neutral, 2 = male, 3 = female
 input$pnhej <- c(0, 0.02) #probability of non-homologous end joining, determines the resistance alleles (0.02 in mosquitos)
-input$cutRate <- c(1, 0.95) #propability CRISPR cuts the opposite DNA strand
-input$pHMort <- c(0, 0.1) #mortality of gene drive carriers.
+input$cutRate <- c(1, 0.95, 0.97) #propability CRISPR cuts the opposite DNA strand
+input$pHMort <- c(0, 0.1, 0.15) #mortality of gene drive carriers.
 inputs <- expand.grid(input)
 
 inputs <- inputs %>%
-  filter(((meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4 & maxMalMatings==2) |
-            (meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4) |
-            (meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & meanMalMatings==0.9 & rmax==10 & maxMalMatings==2) |
-            (meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & meanMalMatings==0.9 & maxFemMatings==4 & maxMalMatings==2) |
-            (meanFemProgeny==300 & meanMalProgeny==300 & meanFemMatings==3.275 & rmax==10 & maxFemMatings==4 & maxMalMatings==2) |
-            (meanFemProgeny==300 & meanMalProgeny==300 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4 & maxMalMatings==2) |
-            (meanFemProgeny==meanMalProgeny & meanFemMatings==3.275 & meanMalMatings==0.9 & rmax==10 & maxFemMatings==4 & maxMalMatings==2)) &
+  filter((meanFemProgeny==meanMalProgeny & meanFemMatings==3.275 & maxFemMatings==4 | 
+           meanFemProgeny==meanMalProgeny & meanFemMatings==0.2 & maxFemMatings==2) &
            ((pnhej == 0 & cutRate == 1 & pHMort == 0) |
-           (pnhej == 0.02 & cutRate == 0.95 & pHMort == 0.1)))
+              (pnhej == 0.02 & cutRate == 0.95 & pHMort == 0.1) |
+              (pnhej == 0 & cutRate == 0.97 & pHMort == 0.15)))
 
 #########################################
 ########## Run model ####################
@@ -65,216 +62,42 @@ modelOutput <- apply(modelOutput, 2, c)
 ########## Save model ###################
 #########################################
 
-save(modelOutput, file = "FigS2.Rdata")
+save(modelOutput, file = "FigS4.Rdata")
 
-##################
-## ---- Plots ----
-##################
+#################### Prepare data ##################
 
-modelOutput <- as_tibble(modelOutput)
-
-modelOutput <- rowwise(modelOutput) %>% 
-  select(-meanMalProgeny) %>%
-  rename(meanProgeny = meanFemProgeny) %>%
-  mutate(variableRange = case_when(
-    meanProgeny != 300 ~ "meanProgeny",
-    meanFemMatings != 3.275 ~ "meanFemMatings",
-    meanMalMatings != 0.9 ~ "meanMalMatings",
-    maxFemMatings != 4 ~ "maxFemMatings",
-    maxMalMatings != 2 ~ "maxMalMatings",
-    rmax != 10 ~ "rmax",
-    TRUE ~ "default"
-  ))
-
-modelOutputOptimal <- filter(modelOutput, cutRate == 1)
-modelOutputRealistic <- filter(modelOutput, cutRate == 0.95)
-
-year25optimal <- filter(modelOutputOptimal, generation == 25)
-year25realistic <- filter(modelOutputRealistic, generation == 25)
-
-ntimes <- nrow(year25optimal)/max(inputs$repetitions)
-year25optimal$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
-year25optimal$paramSet <- factor(year25optimal$paramSet)
-year25realistic$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
-year25realistic$paramSet <- factor(year25realistic$paramSet)
+# modelOutput <- as_tibble(modelOutput)
+# 
+# modelOutput <- rowwise(modelOutput) %>% 
+#   select(-meanMalProgeny) %>%
+#   rename(meanProgeny = meanFemProgeny) %>%
+#   mutate(condition = case_when(
+#     maxFemMatings == 2 ~ "European Paper Wasp",
+#     maxFemMatings == 4 ~ "Asian Hornet",
+#   ))
+# 
+# PaperTheme <- theme_bw(base_size = 11, base_family = "sans") + 
+#   theme(strip.background = element_blank(),
+#         panel.grid = element_blank(),
+#         title=element_text(size=14, hjust=0.5), 
+#         plot.title=element_text(size=14, hjust=0.5),
+#         axis.title=element_text(size=12))
+# 
+# modelOutputOptimal <- filter(modelOutput, cutRate == 1)
+# modelOutputRealistic <- filter(modelOutput, cutRate == 0.95)
+# modelOutputMid <- filter(modelOutput, cutRate == 0.97)
+# 
+# year25optimal <- filter(modelOutputOptimal, generation == 25)
+# year25realistic <- filter(modelOutputRealistic, generation == 25)
+# year25mid<- filter(modelOutputMid, generation == 25)
+# 
+# ntimes <- nrow(year25optimal)/max(inputs$repetitions)
+# year25optimal$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
+# year25optimal$paramSet <- factor(year25optimal$paramSet)
+# year25realistic$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
+# year25realistic$paramSet <- factor(year25realistic$paramSet)
+# year25mid$paramSet <- rep(1:ntimes, each = max(inputs$repetitions))
+# year25mid$paramSet <- factor(year25mid$paramSet)
 
 ################## Realistic GD scenario ##################
-
-suppressed <- rowwise(year25realistic) %>%
-  mutate(suppressed = case_when(popSizeF == 0 ~ 1,
-                                popSizeF > 0 ~ 0)) %>%
-  group_by(paramSet) %>%
-  mutate(suppressionRate = sum(suppressed)/10) %>%
-  distinct(paramSet, .keep_all = TRUE) %>%
-  select(paramSet, meanProgeny:maxMalMatings, rmax, suppressionRate, variableRange) %>%
-  pivot_longer(cols = meanProgeny:rmax) %>%
-  filter(variableRange == name | variableRange == "default") %>%
-  mutate(name = factor(name), cond = "opt") %>%
-  arrange(name)
-
-suppressed$name <- factor(suppressed$name, 
-                          levels = c('rmax','meanFemMatings','meanMalMatings',
-                                     'maxFemMatings','maxMalMatings',
-                                     'meanProgeny', 'default'), 
-                          labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                     'Max female matings','Max male matings',
-                                     'Mean progeny', 'Default'))
-suppressed <- suppressed[order(suppressed$name),]
-
-PaperTheme <- theme_bw(base_size = 11, base_family = "sans") + 
-  theme(strip.background = element_blank(),
-        panel.grid = element_blank(),
-        title=element_text(size=14, hjust=0.5), 
-        plot.title=element_text(size=14, hjust=0.5),
-        legend.position="none",
-        axis.title=element_text(size=12))
-
-p1 <- ggplot(data = suppressed) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = suppressionRate, colour = name)) +
-  geom_point(aes(x = value, y = suppressionRate, colour = name)) +
-  scale_colour_manual(values=rep("black",6)) +
-  ylim(c(0,1)) +
-  ylab("Suppression rate") +
-  xlab("Parameter value") + 
-  PaperTheme +
-  ggtitle("Realistic gene drive")
-p1
-
-modelOutputRealistic$paramSet <- rep(1:ntimes, each = nrow(modelOutputRealistic)/ntimes)
-modelOutputRealistic$paramSet <- factor(modelOutputRealistic$paramSet)
-
-lastGenRealistic <- modelOutputRealistic %>%
-  filter(popSizeF != 0) %>%
-  group_by(paramSet, repetitions) %>%
-  filter(generation == max(generation))
-
-lastGenStatsRealistic <- group_by(lastGenRealistic, paramSet) %>%
-  summarise(mLastGen = mean(generation),
-            semLastGen = sd(generation)/sqrt(10)) 
-
-lastGenStatsRealistic <- left_join(x = lastGenStatsRealistic, 
-                                 y = lastGenRealistic, by = "paramSet") %>%
-  distinct(paramSet, .keep_all = TRUE) %>%
-  select(paramSet:semLastGen, meanProgeny:rmax, -k, variableRange) %>%
-  pivot_longer(cols = meanProgeny:rmax) %>%
-  filter(variableRange == name | variableRange == "default") %>%
-  mutate(name = factor(name, levels = c('rmax','meanFemMatings','meanMalMatings',
-                                        'maxFemMatings','maxMalMatings',
-                                        'meanProgeny', 'default'), 
-                       labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                  'Max female matings','Max male matings',
-                                  'Mean progeny', 'Default')), 
-         variableRange = factor(variableRange, levels = c('rmax','meanFemMatings','meanMalMatings',
-                                                          'maxFemMatings','maxMalMatings',
-                                                          'meanProgeny', 'default'), 
-                                labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                           'Max female matings','Max male matings',
-                                           'Mean progeny', 'Default')),
-         lower = mLastGen - semLastGen,
-         upper = mLastGen + semLastGen)
-lastGenStatsRealistic <- lastGenStatsRealistic[order(lastGenStatsRealistic$name),]
-
-p2 <- ggplot(data = lastGenStatsRealistic) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = mLastGen, colour = name)) +
-  geom_point(aes(x = value, y = mLastGen, colour = name)) +
-  geom_errorbar(aes(x = value, y = mLastGen, ymin = lower, ymax = upper), 
-                size = 0.1, width = 0.1) +
-  ylim(c(0,25)) +
-  ylab("Last viable generation") +
-  xlab("Parameter value") +
-  scale_colour_manual(values=rep("black",6)) +
-  PaperTheme +
-  ggtitle("Realistic gene drive")
-p2
-
-################## Optimal GD scenario ##################
-
-suppressed <- rowwise(year25optimal) %>%
-  mutate(suppressed = case_when(popSizeF == 0 ~ 1,
-                                popSizeF > 0 ~ 0)) %>%
-  group_by(paramSet) %>%
-  mutate(suppressionRate = sum(suppressed)/10) %>%
-  distinct(paramSet, .keep_all = TRUE) %>%
-  select(paramSet, meanProgeny:maxMalMatings, rmax, suppressionRate, variableRange) %>%
-  pivot_longer(cols = meanProgeny:rmax) %>%
-  filter(variableRange == name | variableRange == "default") %>%
-  mutate(name = factor(name), cond = "opt") %>%
-  arrange(name)
-
-suppressed$name <- factor(suppressed$name, 
-                          levels = c('rmax','meanFemMatings','meanMalMatings',
-                                     'maxFemMatings','maxMalMatings',
-                                     'meanProgeny', 'default'), 
-                          labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                     'Max female matings','Max male matings',
-                                     'Mean progeny', 'Default'))
-suppressed <- suppressed[order(suppressed$name),]
-
-p3 <- ggplot(data = suppressed) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = suppressionRate, colour = name)) +
-  geom_point(aes(x = value, y = suppressionRate, colour = name)) +
-  scale_colour_manual(values=rep("black",6)) +
-  ylim(c(0,1)) +
-  ylab("Suppression rate") +
-  xlab("Parameter value") + 
-  PaperTheme +
-  ggtitle("Optimal gene drive")
-p3
-
-modelOutputOptimal$paramSet <- rep(1:ntimes, each = nrow(modelOutputOptimal)/ntimes)
-modelOutputOptimal$paramSet <- factor(modelOutputOptimal$paramSet)
-
-lastGenOptimal <- modelOutputOptimal %>%
-  filter(popSizeF != 0) %>%
-  group_by(paramSet, repetitions) %>%
-  filter(generation == max(generation))
-
-lastGenStatsOptimal <- group_by(lastGenOptimal, paramSet) %>%
-  summarise(mLastGen = mean(generation),
-            semLastGen = sd(generation)/sqrt(10)) 
-
-lastGenStatsOptimal <- left_join(x = lastGenStatsOptimal, 
-                             y = lastGenOptimal, by = "paramSet") %>%
-  distinct(paramSet, .keep_all = TRUE) %>%
-  select(paramSet:semLastGen, meanProgeny:rmax, -k, variableRange) %>%
-  pivot_longer(cols = meanProgeny:rmax) %>%
-  filter(variableRange == name | variableRange == "default") %>%
-  mutate(name = factor(name, levels = c('rmax','meanFemMatings','meanMalMatings',
-                                        'maxFemMatings','maxMalMatings',
-                                        'meanProgeny', 'default'), 
-                       labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                  'Max female matings','Max male matings',
-                                  'Mean progeny', 'Default')), 
-         variableRange = factor(variableRange, levels = c('rmax','meanFemMatings','meanMalMatings',
-                                                          'maxFemMatings','maxMalMatings',
-                                                          'meanProgeny', 'default'), 
-                                labels = c('Max growth rate','Mean female matings','Mean male matings',
-                                           'Max female matings','Max male matings',
-                                           'Mean progeny', 'Default')),
-         lower = mLastGen - semLastGen,
-         upper = mLastGen + semLastGen)
-lastGenStatsOptimal <- lastGenStatsOptimal[order(lastGenStatsOptimal$name),]
-
-p4 <- ggplot(data = lastGenStatsOptimal) +
-  facet_wrap(. ~ name, scales = "free_x") +
-  geom_line(aes(x = value, y = mLastGen, colour = name)) +
-  geom_point(aes(x = value, y = mLastGen, colour = name)) +
-  geom_errorbar(aes(x = value, y = mLastGen, ymin = lower, ymax = upper), 
-                size = 0.1, width = 0.1) +
-  ylim(c(0,25)) +
-  ylab("Last viable generation") +
-  xlab("Parameter value") +
-  scale_colour_manual(values=rep("black",6)) +
-  PaperTheme +
-  ggtitle("Optimal gene drive")
-p4
-
-p <- (p1 + p2) / (p3 + p4) + 
-  plot_annotation(tag_levels = "A")
-p
-
-ggsave(plot = p, filename = "FigS2.png", height = 30, width = 25, unit = "cm")
 
