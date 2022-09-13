5e7dbe13a693a4de81880d28c4ff2ecef99877e6
Yani Meiborg <ymeiborg@bell-fe00.rcac.purdue.edu>
2022-09-13 12:06:52 -0400

    added scripts to run pilot fig 2

---
+++ test2/test2_6.R
@@ -0,0 +1,57 @@
+##########################
+########## Setup #########
+##########################
+
+setwd("/scratch/bell/ymeiborg/ymeiborg_hornet_gd/test2")
+source("../model_function.R")
+
+#########################################
+########## Set input parameters #########
+#########################################
+
+input <- list()
+
+input$repetitions <- 1 # number of reps
+input$generations <- 25 #runtime of the simulation
+input$meanFemProgeny <- 300 #average of female progeny per queen
+input$meanMalProgeny <- 300 #average of male progeny per queen
+input$meanFemMatings <- 3.275 #average number of times females mate
+input$meanMalMatings <- 0.9 #average number of times males mate
+input$maxFemMatings <- 4 #maximum number of times females mate
+input$maxMalMatings <- 2 #maximum number of times males mate
+input$k <- 1000 #simulated k --> carrying capacity (Carrying capacity K equals to 10.6/km^2)
+input$rmax <- 10 #growth rate of the population
+input$N <- 1000 #size of start WT population
+input$gdSex <- "F" #which sex carries the gene drive F or M
+input$nGD <- 100 #number of gene drive carrying animals to introduce
+input$strategy <- 3 #what targeting strategy to use 1 = neutral, 2 = male, 3 = female
+input$pnhej <- c(0, 0.02) #probability of non-homologous end joining, determines the resistance alleles (0.02 in mosquitos)
+input$cutRate <- seq(0.8, 1, 0.05) #probability CRISPR cuts the opposite DNA strand
+input$pHMort <- seq(0, 0.5, 0.1) #mortality of gene drive carriers.
+input$pFunctionalRepair <- c(0.078) #probability a resistance allele forms after non-homologous end-joining.
+inputs <- expand.grid(input)
+
+#########################################
+########## Run model ####################
+#########################################
+
+# Track population information
+generation <- 0:input$generations
+var <- c('popSizeF', 'WT', 'GD', 'NF', 'RE', 'homoWT', 'homoGD', 'heteroGD')
+modelOutput <- array(NA,
+                     dim=c(length(generation),(length(var)+ncol(inputs)+1), nrow(inputs)),
+                     dimnames=list(generation=generation, var=c("generation",colnames(inputs),var) , run=1:nrow(inputs)))
+
+for (row in 1:nrow(inputs)){
+  results <- modelHornets.comp(inputs[row,])
+  modelOutput[,,row] <- unlist(results)
+}
+
+modelOutput <- apply(modelOutput, 2, c)
+
+#########################################
+########## Save model ###################
+#########################################
+
+save(modelOutput, file = "Test2_6.Rdata")
+
